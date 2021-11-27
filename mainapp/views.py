0114217a508693dbfd4327b2254e0951cb631cd8
Win_victor <win-victor@mail.ru>
2021-11-27 21:59:15 +0300

    the project is configured to work with media files, Products and ProductsCategory models created, superuser created, configured to work with models, Implemented automatic generation of the category menu based on data from the model, Created URL manager in app

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,62 +5,36 @@ from datetime import datetime
 
 from django.conf import settings
 
+from mainapp.models import Product, ProductCategory
+
 
 def index(request):
+    products_list = Product.objects.all()[:4]
+
     context = {
         'title': 'магазин',
         'date': datetime.now(),
+        'products': products_list,
     }
     return render(request, 'mainapp/index.html', context)
 
+#
+# links_menu = [
+#     {'link_name': 'home', 'name': 'Дом'},
+#     {'link_name': 'modern', 'name': 'Модерн'},
+#     {'link_name': 'office', 'name': 'Офис'},
+#     {'link_name': 'classic', 'name': 'Классика'},
+# ]
 
-links_menu = [
-    {'href': 'products', 'name': 'Все'},
-    {'href': 'products_home', 'name': 'Дом'},
-    {'href': 'products_modern', 'name': 'Модерн'},
-    {'href': 'products_office', 'name': 'Офис'},
-    {'href': 'products_classic', 'name': 'Классика'},
-]
 
-
-def products(request):
+def products(request, pk=None):
+    links_menu = ProductCategory.objects.all()
     context = {
         'links_menu': links_menu,
         'title': 'товары',
     }
     return render(request, 'mainapp/products.html', context)
 
-
-def products_home(request):
-    context = {
-        'links_menu': links_menu,
-        'title': 'товары',
-    }
-    return render(request, 'mainapp/products.html', context)
-
-
-def products_modern(request):
-    context = {
-        'links_menu': links_menu,
-        'title': 'товары',
-    }
-    return render(request, 'mainapp/products.html', context)
-
-
-def products_office(request):
-    context = {
-        'links_menu': links_menu,
-        'title': 'товары',
-    }
-    return render(request, 'mainapp/products.html', context)
-
-
-def products_classic(request):
-    context = {
-        'links_menu': links_menu,
-        'title': 'товары',
-    }
-    return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
