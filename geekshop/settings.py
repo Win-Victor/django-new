f99f5334e5ffe21029f07feb46d70743fccb77bc
Win_victor <win-victor@mail.ru>
2022-01-18 20:42:47 +0300

    settings

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k3s)f@nj0qf%u7%8zgef%afu(d=8vvz^sl(ew^%^@#-*qjgdga
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -86,10 +86,18 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'postres'
     }
 }
 
