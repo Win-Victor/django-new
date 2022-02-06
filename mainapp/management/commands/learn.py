9db937eacdd36d95c3f290bffc48dae85cf611ca
Win_victor <win-victor@mail.ru>
2022-02-06 23:23:13 +0300

    modern learn

--- mainapp/management/commands/learn.py
+++ mainapp/management/commands/learn.py
@@ -6,6 +6,7 @@ from mainapp.models import Product, ProductCategory
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        product_list = Product.objects.filter(Q(category__name='дом') | Q(category__name='офис'))
+        # product_list = Product.objects.filter(Q(category__name='дом') | Q(category__name='офис'))
+        product_list = Product.objects.filter(category__name__in=['дом', 'офис'])
 
         print(product_list)
