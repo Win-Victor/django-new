a93cacbf67ac50aee174e61ca8ee9c78a4666d58
Win_victor <win-victor@mail.ru>
2022-02-06 22:08:23 +0300

    modified basket_add, ProductCategoryForm

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import F
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
@@ -25,7 +26,8 @@ def basket_add(request, pk):  # pk - Product pk
     if not basket_item:
         basket_item = Basket(user=request.user, product=product_item)
 
-    basket_item.quantity += 1
+    # basket_item.quantity += 1
+    basket_item.quantity = F('quantity') + 1
     basket_item.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
