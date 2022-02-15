6b662adf2bc8a75c6c3eedda97ab3b9c3cf3faad
Win_victor <win-victor@mail.ru>
2022-02-15 17:20:18 +0300

    added tests

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -4,6 +4,7 @@ from mainapp.models import Product, ProductCategory
 
 
 class MainappSmokeTest(TestCase):
+    status_code_success = 200
 
     def setUp(self) -> None:
         category = ProductCategory.objects.create(
@@ -16,12 +17,16 @@ class MainappSmokeTest(TestCase):
                 description=f'description for prod #{i}'
             )
 
-        # self.client = Client()
-
-
     def test_mainapp_urls(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 305)
+        self.assertEqual(response.status_code, self.status_code_success)
 
-    def tearDown(self) -> None:
-        pass
+    def test_categories_urls(self):
+        for cat in ProductCategory.objects.all():
+            response = self.client.get(f'/products/{cat.pk}/')
+            self.assertEqual(response.status_code, self.status_code_success)
+
+    def test_products_urls(self):
+        for prod in Product.objects.all():
+            response = self.client.get(f'/products/product/{prod.pk}/')
+            self.assertEqual(response.status_code, self.status_code_success)
